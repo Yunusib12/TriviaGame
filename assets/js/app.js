4a1d8d507b834c78b705ebe74dc8898067ca9c0d
Ibrahim Yunus <younis.ib@gmail.com>
2019-07-28 17:01:04 -0400

    display question and some details about it

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 $(function() {
 
     let receivedToken = [];
+    let questionArray = [];
     let tokenUrl = "https://opentdb.com/api_token.php?command=request";
     let getQuestions = "https://opentdb.com/api.php?amount=5&token=";
     let giphyUrl = "http://api.giphy.com/v1/gifs/search?q=";
@@ -8,9 +9,22 @@ $(function() {
     let $pickCategory = $("#pickCategory");
     let $genCat = $("#genCat");
     let $main = $("#main");
+    let $timeLeft = $("#timeLeft");
+    let $replyQuestion = $("#replyQuestion");
+    let $displayQuestion = $("#displayQuestion");
+    let $displayCategory = $("#displayCategory");
+    let $displayDifficulty = $("#displayDifficulty");
+    let $displayType = $("#displayType");
+    let seconds = 30;
+    let game;
+    let counter = 0;
+    let clock;
+    let correctAnswer = 0;
+    let incorrectAnswer = 0;
+    let unansweredQuestion = 0;
 
 
-
+    //Function get Question from OpendDB Trivia
 
     let searchTriviaQ = function() {
 
@@ -18,7 +32,7 @@ $(function() {
 
         querySession.done(function(session) {
 
-            receivedToken.push(session.token);
+            receivedToken.push(session.token); // save the token
 
         });
 
@@ -26,15 +40,18 @@ $(function() {
 
         $.get(getQuestions + receivedToken)
             .then(function(response) {
-                createCategory(response);
+
+                createCategory(response); //create category from the response
+
+                questionArray.push(response.results); //Add the data to a question array
             });
     };
+
+    // Function that create category from the Question retrieved 
 
     var createCategory = function(dataCat) {
 
         let categoryData = dataCat.results;
-
-        console.log(categoryData);
 
         categoryData.forEach(function(catData) {
 
@@ -69,110 +86,94 @@ $(function() {
 
     };
 
-    $main.on("click", function(event) {
+    // Function that display question from the selected category
+
+    $main.on("click", ".card", function(event) {
 
         const _this = $(this);
 
         let categoryValue = _this.attr("data-category");
 
-        console.log(event);
+        $pickCategory.hide();
 
-        console.log(categoryValue);
+        displayQuestion(categoryValue);
+
 
     });
 
+    // Wait while loading data
+
+    let waitLoading = function() {
+
+        setTimeout(function() {
+            let imgUrl = "./assets/images/comets-loading.svg";
+
+            let loadimg = $("<img>")
+                .attr("src", imgUrl);
+
+            $main.append(loadimg);
+
+        }, 1000);
+    }
+
+    // Function display Question Category Selected 
+
+    let displayQuestion = function(categorySelected) {
+
+        // displaying the question <div>
+        $replyQuestion.show();
+
+        console.log(categorySelected);
+
+        let qD = $(questionArray); // transorming the array into an jQuery object
+        let qDCat = qD[0]; // accessing the array when i created the array from the response.results created a key:0 + then the array
+        console.log(qDCat);
+
+        // searching in the array the category that the user selected
+        let getCatQ = qDCat.find(catCont => catCont.category === categorySelected);
+
+        // saving the question data into variables
+        let displayQ = getCatQ.question;
+        let displayCat = getCatQ.category;
+        let displayDiff = getCatQ.difficulty;
+        let displayType = getCatQ.type;
+
+        // displaying the question and other infos related
+        $displayQuestion.text(displayQ);
+        $displayCategory.text(`Category: ${displayCat}`);
+        $displayDifficulty.text(`Difficulty: ${displayDiff}`);
+        $displayType.text(`Type: ${displayType}`);
+
+        console.log(displayQ);
+        console.log(displayCat);
+        console.log(displayDiff);
+        console.log(displayType);
+
+    }
+
+    //Function Count Down for the user to pick the right answer
+    function countdown() {
+
+        seconds--;
+
+        // console.log(seconds);
+
+        $timeLeft.text(seconds);
+
+        if (seconds === 0) {
+            clearTimeout(timer)
+            return false;
+        }
+
+        setTimeout(countdown, 1000);
+
+    }
 
 
-
-
-    // let getGifImg = function(catName) {
-
-
-    // };
 
     searchTriviaQ();
-    // if (qT.response_code != 4) {
-
-    //     console.log("Session still on", qT);
-
-
-    // };
-
-
-    // let queryToken = function() {
-
-    //     let qT = $.get(tokenUrl);
-
-    //     qT.done(function(session) {
-    //         receivedToken = session.token
-    //             // console.log("Inside", session.token);
-    //     });
-    // };
-
-    // let querySession = $.get(tokenUrl);
-
-    // querySession.done(function(session) {
-
-    //     receivedToken = session.token;
-    //     console.log("My Session", receivedToken);
-
-    // }).then(function() {
-
-    //     let queryUrl = $.get(getQuestions + receivedToken);
-
-    //     queryUrl.done(function(question) {
-
-    //         console.log("Got some questions ", question);
-
-    //         getCategory(question);
-
-    //     });
-    // });
-
-    // let getCategory = function(data) {
-    //     console.log("Get Cat", data.results.category);
-    // }
-
-
-
-    //queryToken();
-
-    //console.log("My Token", receivedToken);
-    //console.log("Outside", results.category);
-
-
-
-
-
-
-    // let token;
-
-    // let querySession = $.get("");
-
-    // querySession.done(function(session) {
-
-    //     token = session.token;
-    //     console.log("My Session", token);
-
-    // }).then(function() {
-
-    //     let queryUrl = $.get("https://opentdb.com/api.php?amount=10&token=" + token);
-
-    //     queryUrl.done(function(question) {
-
-    //         console.log("Got some questions ", question);
-
-    //     });
-    // });
-
-
-
-    // let weather = $.get("https://api.openweathermap.org/data/2.5/weather?q=Portland,US&APPID=23f01da04b864d241e7b00caec810037");
-
-    // weather.done(function(datas) {
-
-    //     console.log("My weather datas", datas);
-    // });
+    $replyQuestion.hide();
+    const timer = setTimeout(countdown, 1000);
 
 
 });
